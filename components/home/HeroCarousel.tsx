37e8ffeadca39ad350577a320868e185f7eead35
Boitumelo <mkwanazibuti082@gmail.com>
2024-08-31 22:59:24 +0200

    Hero Carousel

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import hero1 from "@/public/images/hero01.jpg";
+import hero2 from "@/public/images/hero02.jpg";
+
+const carouselImages = [hero1, hero2];
 
 function HeroCarousel() {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="p-2">
+                    <Image
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
-
 export default HeroCarousel;
